e76016b8be3e9fca728f27dd5136262f5e39df0b
uday <patiludayk@gmail.com>
2022-09-21 00:05:42 +0100

    uday1: initial commit

---
+++ src/main/java/com/uday/leetcode/psds/PsdsApplication.java
@@ -0,0 +1,13 @@
+package com.uday.leetcode.psds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uday/leetcode/psds/PsdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uday.leetcode.psds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
