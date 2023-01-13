dd3813d04b87faa2aafbb06ce267440edac564da
patiludayk <patiludayk@gmail.com>
2023-01-13 14:57:04 +0000

    uday5: adding new problems

---
+++ src/main/java/com/uday/practise/ps/array/ValidAnagram.java
@@ -0,0 +1,44 @@
+package com.uday.practise.ps.array;
+
+import java.util.Arrays;
+
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length, t.length <= 5 * 104
+ * s and t consist of lowercase English letters.
+ *
+ *
+ * Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+ */
+public class ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+
+    private static boolean isAnagram(String s, String t) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return Arrays.equals(sArr, tArr);
+    }
+}
